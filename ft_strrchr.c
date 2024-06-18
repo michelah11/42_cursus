e36134593ccf87790b7a924cb1e79ad906c9852e
Michel Abou Haidar <mabou-ha@c1r2s6.42beirut.com>
2024-06-18 22:52:53 +0300

    libft

---
+++ ft_strrchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabou-ha <mabou-ha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 15:36:42 by mabou-ha          #+#    #+#             */
+/*   Updated: 2024/06/10 18:24:02 by mabou-ha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *str, int c)
+{
+	const char	*last = NULL;
+
+	while (*str)
+	{
+		if (*str == (char)c)
+			last = str;
+		str++;
+	}
+	if (c == '\0')
+		return ((char *)str);
+	return ((char *)last);
+}

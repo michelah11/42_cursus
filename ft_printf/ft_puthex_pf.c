e2a1895fd26f4c5bddbf9a088801404f40292047
Michel Abou Haidar <mabou-ha@c1r3s3.42beirut.com>
2024-06-22 04:41:45 +0300

    .

--- ft_printf/ft_puthex_pf.c
+++
@@ -1,4 +0,0 @@
-#include "ft_printf.h"
-#include "libft.h"
-
-int	ft_puthex_pf(unsigned int num, char *base);
--- ft_printf/ft_putuint_pf.c
+++
@@ -1,5 +0,0 @@
-#include "ft_printf.h"
-#include "libft.h"
-
-void	ft_putuint_fd(unsigned int num);
-int	ft_putuint_pf(unsigned int num);

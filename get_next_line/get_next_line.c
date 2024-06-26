ab766617c216406c44b56b4ced5682b78faed455
Michel Abou Haidar <mabou-ha@c1r9s5.42beirut.com>
2024-06-26 22:21:09 +0300

    .

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,28 +6,29 @@
 /*   By: mabou-ha <mabou-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 22:13:26 by mabou-ha          #+#    #+#             */
-/*   Updated: 2024/06/18 18:51:20 by mabou-ha         ###   ########.fr       */
+/*   Updated: 2024/06/26 22:18:11 by mabou-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*set_line(char *line_buffer)
+char	*set_line(char *ln_bf)
 {
 	char	*left_sub;
 	int		i;
 
 	i = 0;
-	while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
+	while (ln_bf[i] != '\n' && ln_bf[i] != '\0')
 		i++;
-	if (line_buffer[i] == '\0' || line_buffer[1] == '\0')
+	if (ln_bf[i] == '\0')
 		return (NULL);
-	left_sub = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - i - 1);
-	if (!left_sub)
+	else
 	{
-		return (NULL);
+		left_sub = ft_substr(ln_bf, i + 1, ft_strlen(ln_bf) - i - 1);
+		if (!left_sub)
+			return (NULL);
+		ln_bf[i + 1] = '\0';
 	}
-	line_buffer[i + 1] = '\0';
 	return (left_sub);
 }
 
@@ -45,15 +46,18 @@ char	*fill_line_buffer(int fd, char *left_sub, char *buffer)
 			free (left_sub);
 			return (NULL);
 		}
-		else if (b_read == 0)
+		else if (b_read == 0 && (!left_sub || left_sub[0] == '\0'))
+		{
+			free (left_sub);
+			left_sub = NULL;
 			break ;
+		}
 		buffer[b_read] = '\0';
 		if (!left_sub)
 			left_sub = ft_strdup("");
 		temp = left_sub;
 		left_sub = ft_strjoin(temp, buffer);
 		free (temp);
-		temp = NULL;
 		if (!left_sub)
 			return (NULL);
 		if (ft_strchr(buffer, '\n'))
@@ -86,5 +90,7 @@ char	*get_next_line(int fd)
 	if (!line)
 		return (NULL);
 	left_sub = set_line(line);
+	if (line && line[0] == '\0' && !left_sub)
+		return (ft_strdup(""));
 	return (line);
 }
---
+++ get_next_line/main.c
@@ -0,0 +1,32 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
+int main(int argc, char **argv)
+{
+    int fd;
+    char *line;
+
+    if (argc != 2)
+    {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd < 0)
+    {
+        perror("Error opening file");
+        return 1;
+    }
+
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s", line);
+        free(line);
+    }
+
+    close(fd);
+    return 0;
+}
